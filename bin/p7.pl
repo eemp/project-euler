c01b8a52fd294cd3bc732bdeedc0530690c7b2ff
eemp <ee.mihir.patel@gmail.com>
2015-06-19 09:21:59 -0400

    solution for problem # 7

---
+++ bin/p7.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use feature qw(say);
+use FindBin qw($Bin);
+use Data::Dumper;
+
+use lib "$Bin/../lib";
+
+=pod
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+What is the 10 001st prime number?
+=cut
+
+my $SIEVE_SIZE = $ARGV[0] || 1_000_000;
+
+my $solution;
+
+## Erathosthenes Sieve
+my @primes = ();
+my @sieve = ();
+for(my $k = 0; $k < $SIEVE_SIZE; $k++) { push(@sieve, $k); }
+
+for(my $k = 2; $k < scalar @sieve; $k++)
+{
+    if($sieve[$k] != 0)
+    {
+        push(@primes, $k);
+        for(my $l = $k; $l < scalar @sieve; $l+=$k)
+        {
+            $sieve[$l] = 0;
+        }
+    }
+}
+
+$solution = $primes[10_001];
+
+say "Solution = $solution" if $solution; # 104759
+
