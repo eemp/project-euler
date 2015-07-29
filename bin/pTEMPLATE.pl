96939f298d0c092aa58b24f14541618398325ceb
Mihir Patel <ee.mihir.patel@gmail.com>
2015-07-29 09:27:47 -0400

    added a template script as a starting point

---
+++ bin/pTEMPLATE.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use feature qw(say);
+use Data::Dumper;
+
+use lib "$Bin/../lib";
+
+=pod
+=cut
+
+my $solution;
+say "Solution = $solution" if $solution; #  
+
